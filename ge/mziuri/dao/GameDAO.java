68bb512152ffb41cdc5a7ae4ad984feb59542721
getsadzeg <getsadzeguram@gmail.com>
2016-11-16 21:56:35 +0400

    fill game DAO interface in

--- ge/mziuri/dao/GameDAO.java
+++ ge/mziuri/dao/GameDAO.java
@@ -1,7 +1,15 @@
 
 package ge.mziuri.dao;
 
+import ge.mziuri.model.Game;
+import java.util.ArrayList;
+import java.util.Date;
 
 public interface GameDAO {
-    
+    void addGame(Game game);
+    void deleteGame(int gameID);
+    void editGame(int gameID);
+    ArrayList<Game> aboutGames();
+    Date getDate(int gameID);
+    ArrayList<Integer> getCoefficient(String team);
 }
