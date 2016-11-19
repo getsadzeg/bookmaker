3ff372c737563235415d022fbc26e95d245e2602
getsadzeg <getsadzeguram@gmail.com>
2016-11-19 14:01:32 +0400

    change arguments of editGame

--- ge/mziuri/dao/GameDAO.java
+++ ge/mziuri/dao/GameDAO.java
@@ -8,7 +8,7 @@ import java.util.Date;
 public interface GameDAO {
     void addGame(Game game);
     void deleteGame(int gameID);
-    void editGame(int gameID);
+    void editGame(int oldgameID, Game game);
     ArrayList<Game> aboutGames();
     Date getDate(int gameID);
     ArrayList<Integer> getCoefficient(String team);
