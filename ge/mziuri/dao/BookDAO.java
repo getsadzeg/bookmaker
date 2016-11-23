1ee6d205acc4c32144aba1dbdcbd97a46ce52e86
getsadzeg <getsadzeguram@gmail.com>
2016-11-23 14:45:30 +0400

    refactor BookDAO interface

--- ge/mziuri/dao/BookDAO.java
+++ ge/mziuri/dao/BookDAO.java
@@ -6,11 +6,8 @@ import ge.mziuri.model.Book;
 
 
 public interface BookDAO {
-    void CreateBook(Book book);
-    void DeleteBook(int bookID);
-    void addGameBet(Bet bet);
-    void DeleteGameBet(Bet bet);
-    void Winner();
-
+    void createBook(Book book);
+    void deleteBook(int bookID);
+    boolean isWinner();
 }
    
