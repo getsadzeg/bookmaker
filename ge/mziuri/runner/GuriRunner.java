536430beda2de474c2b1d9466d8fff87cbe44b50
getsadzeg <getsadzeguram@gmail.com>
2016-11-24 18:06:32 +0400

    start testing

--- ge/mziuri/runner/GuriRunner.java
+++ ge/mziuri/runner/GuriRunner.java
@@ -1,9 +1,22 @@
 
 package ge.mziuri.runner;
 
+import ge.mziuri.dao.BetDAO;
+import ge.mziuri.dao.BetDAOImpl;
+import ge.mziuri.dao.BookDAO;
+import ge.mziuri.dao.BookDAOImpl;
+import ge.mziuri.dao.GameDAO;
+import ge.mziuri.dao.GameDAOImpl;
+import ge.mziuri.model.Game;
+import java.util.Date;
+
 
 public class GuriRunner {
     public static void main(String[] args) {
+        BetDAO betdao = new BetDAOImpl();
+        GameDAO gamedao = new GameDAOImpl();
+        BookDAO bookdao = new BookDAOImpl();
+        gamedao.addGame(new Game(1, new Date(), "Barcelona", "Manchester UTD", 1.7, 2.8));
         
     }
 }
