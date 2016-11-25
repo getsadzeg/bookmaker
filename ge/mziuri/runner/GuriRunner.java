ca922b6daa43fafc5e431538fcc004bc18cedfe5
getsadzeg <getsadzeguram@gmail.com>
2016-11-26 00:44:10 +0400

    test bet methods

--- ge/mziuri/runner/GuriRunner.java
+++ ge/mziuri/runner/GuriRunner.java
@@ -7,16 +7,44 @@ import ge.mziuri.dao.BookDAO;
 import ge.mziuri.dao.BookDAOImpl;
 import ge.mziuri.dao.GameDAO;
 import ge.mziuri.dao.GameDAOImpl;
+import ge.mziuri.enums.RESULT;
+import ge.mziuri.exception.IncorrectGameException;
+import ge.mziuri.exception.NoSuchTeamException;
+import ge.mziuri.model.Bet;
+import ge.mziuri.model.Book;
 import ge.mziuri.model.Game;
+import java.util.ArrayList;
 import java.util.Date;
 
 
 public class GuriRunner {
     public static void main(String[] args) {
+        Game game = new Game(1, new Date(), "Barcelona", "Manchester UTD", 1.7, 2.8);
+        Game game1 = new Game(2, new Date(), "Real Madrid", "Arsenal", 1.5, 3);
+        Bet bet = new Bet(1, 1, "Barcelona", game, RESULT.WON);
+        Bet bet1 = new Bet(2, 1, "Arsenal", game1, RESULT.LOST);
+        ArrayList bets = new ArrayList<>();
+        bets.add(bet);
+        bets.add(bet1);
+        Book book = new Book(bets, 50);
         BetDAO betdao = new BetDAOImpl();
         GameDAO gamedao = new GameDAOImpl();
         BookDAO bookdao = new BookDAOImpl();
-        gamedao.addGame(new Game(1, new Date(), "Barcelona", "Manchester UTD", 1.7, 2.8));
+        bookdao.createBook(book);
+        betdao.addBet(bet);
+       // betdao.getResult(1);
+        //betdao.deleteBet(1);
+        //gamedao.addGame(game);
+        //gamedao.addGame(game1);
         
+        
+        /* try {
+            System.out.println(gamedao.getCoefficient("Barcelona", game));
+        }
+        catch(IncorrectGameException | NoSuchTeamException ex) {
+            System.out.println(ex.getMessage());
+        } */
+        /* System.out.println(gamedao.getDate(1).toString());
+        System.out.println(gamedao.aboutGames()); */
     }
 }
