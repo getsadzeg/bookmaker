fe473aa3fc24f52fd3dda39078e8ab68fd0f4ddf
getsadzeg <getsadzeguram@gmail.com>
2016-12-03 15:59:12 +0400

    fix a lot

--- ge/mziuri/network/Client.java
+++ ge/mziuri/network/Client.java
@@ -1,13 +1,6 @@
-
 package ge.mziuri.network;
 
-import ge.mziuri.dao.GameDAO;
-import ge.mziuri.dao.GameDAOImpl;
-import ge.mziuri.exception.IncorrectGameException;
-import ge.mziuri.exception.NoSuchTeamException;
 import ge.mziuri.model.Game;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -18,144 +11,131 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
-
 public class Client {
+
     static ObjectInputStream in = null;
     static ObjectOutputStream out = null;
     static Socket socket = null;
     static Object[] obj = null;
-    
-    public static void main(String[] args) {
+    static Object[] answers = new Object[3];
+
+    public static void main(String[] args) throws IOException {
         try {
-            
             socket = new Socket("localhost", 8659);
-            in = new ObjectInputStream(socket.getInputStream());
             out = new ObjectOutputStream(socket.getOutputStream());
-            Scanner scanner = new Scanner(System.in);
-            Thread thread = new Thread(){
-                
-                @Override
-                public void run() {
+            in = new ObjectInputStream(socket.getInputStream());
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+            return;
+        }
+
+        Thread thread = new Thread() {
+
+            @Override
+            public void run() {
+                while (true) {
                     try {
-                        while (true) {
-                            GameDAOImpl gamedao = new GameDAOImpl();
-                            obj = (Object[]) in.readObject();
-                            switch((String)obj[0]) {
-                                case "game 1": 
-                                    gamedao.addGame((Game)obj[1]);
-                                    break;
-                                case "game 2":
-                                    gamedao.deleteGame((Integer)obj[1]);
-                                    break;
-                                case "game 3":
-                                    gamedao.editGame(Integer.parseInt((String)obj[1]), (Game)obj[2]);
-                                case "game 4":
-                                    System.out.println(gamedao.aboutGames());
-                                    break;
-                                case "game 5":
-                                    System.out.println(gamedao.getDate((Integer)obj[1]));
-                                    break;
-                                case "game 6":
-                                    try {
-                                        System.out.println(gamedao.getCoefficient((String)obj[1], (Game)obj[2]));
-                                    }
-                                    catch(IncorrectGameException | NoSuchTeamException ex) {
-                                        System.out.println(ex.getMessage());
-                                    }
+                        answers = (Object[]) in.readObject();
+                        switch ((String) answers[0]) {
+                            case "game 1":
+                            case "game 2":
+                            case "game 3":
+                                System.out.println(answers[0]);
+                                System.out.println(answers[1]);
                                 break;
-                                    
-                                    
-                            }
+                            case "game 4":
+                            case "game 5":
+                            case "game 6":
+                                System.out.println(answers[0]);
+                                System.out.println(answers[1]);
+                                System.out.println(answers[2]);
+                                break;
+
                         }
-                    } catch(IOException | ClassNotFoundException ex) {
+                        //  System.out.println(answers[0]);
+                        //  if(answers[1] != null) System.out.println(answers[1]);
+                    } catch (IOException | ClassNotFoundException ex) {
                         System.out.println(ex.getMessage());
                     }
                 }
-            };
-            thread.start();
-            System.out.println("1 - add game"
-                                    + System.lineSeparator() + "2 - delete game"
-                                    + System.lineSeparator() + "3 - edit game"
-                                    + System.lineSeparator() + "4 - about games"
-                                    + System.lineSeparator() + "5 - get date"
-                                    + System.lineSeparator() + "6 - get coefficient");
-            while (scanner.hasNextLine()) {
-                
-                
-                
+            }
+        };
+        thread.start();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(
+                "1 - add game"
+                + System.lineSeparator() + "2 - delete game"
+                + System.lineSeparator() + "3 - edit game"
+                + System.lineSeparator() + "4 - about games"
+                + System.lineSeparator() + "5 - get date"
+                + System.lineSeparator() + "6 - get coefficient");
+        while (scanner.hasNextLine()) {
+
+            try {
                 String text = scanner.nextLine();
-                if(text.contains("game 1")) {
+                if (text.contains("game 1")) {
                     out.writeObject("game 1");
                     out.writeObject(processGame(text));
-                }
-                else if(text.contains("game 2")) {
+                } else if (text.contains("game 2")) {
                     out.writeObject("game 2");
                     out.writeObject(getID(text));
                     out.writeObject(processGame(text));
-                }
-                else if(text.contains("game 3")) {
+                } else if (text.contains("game 3")) {
                     out.writeObject("game 3");
                     out.writeObject(getAdditionalParam(text)); // oldGameID
                     out.writeObject(processGame(text)); //game obj
-                }
-                else if(text.contains("game 4")) {
+                } else if (text.contains("game 4")) {
                     out.writeObject("game 4");
-                }
-                
-                else if(text.contains("game 5")) {
+                } else if (text.contains("game 5")) {
                     out.writeObject("game 5");
                     out.writeObject(getID(text));
-                }
-                else if(text.contains("game 6")) {
+                } else if (text.contains("game 6")) {
                     out.writeObject("game 6");
                     out.writeObject(getAdditionalParam(text)); //team
                     out.writeObject(processGame(text)); //game obj
-                }
-                
-                
-                else if (text.equals("exit")) { 
+                } else if (text.equals("exit")) {
                     break;
                 }
-            }
-        } catch(IOException ex) {
-            System.out.println(ex.getMessage());
-        } finally {
-            try {
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            } finally {
                 in.close();
                 out.close();
                 socket.close();
-            } catch (IOException ex) {
-                System.out.println(ex.getMessage());
             }
         }
-        
     }
-    
+
     public static Game processGame(String args) {
-                    String[] splitted = args.split("\\s+");
-                    Integer id = Integer.parseInt(splitted[2]);
-                    DateFormat formatter; 
-                    Date date = null;
-                    formatter = new SimpleDateFormat("dd-MMM-yy");
-                     try {
-                         date = formatter.parse(splitted[3]);
-                     } 
-                     catch(ParseException ex) {
-                         System.out.println(ex.getMessage());
-                     }
-                     String team1 = splitted[4];
-                     String team2 = splitted[5];
-                     Integer coefficient1 = Integer.parseInt(splitted[6]);
-                     Integer coefficient2 = Integer.parseInt(splitted[7]);
-                     
-                     return new Game(id.intValue(), date, team1, team2, coefficient1.doubleValue(), coefficient2.doubleValue());
+        Double coefficient1 = null;
+        Double coefficient2 = null;
+        String[] splitted = args.split(" ");
+        Integer id = Integer.parseInt(splitted[2]);
+        DateFormat formatter;
+        Date date = null;
+        formatter = new SimpleDateFormat("dd-mm-yyyy");
+        try {
+            date = formatter.parse(splitted[3]);
+        } catch (ParseException ex) {
+            System.out.println(ex.getMessage());
+        }
+        String team1 = splitted[4];
+        String team2 = splitted[5];
+        try {
+            coefficient1 = Double.parseDouble(splitted[6]);
+            coefficient2 = Double.parseDouble(splitted[7]);
+        } catch(NumberFormatException ex) {
+            
+        }
+        return new Game(id, date, team1, team2, coefficient1.doubleValue(), coefficient2.doubleValue());
     }
-    
+
     public static int getID(String args) {
         String[] splitted = args.split("\\s+");
         return Integer.parseInt(splitted[2]);
     }
-    
+
     public static String getAdditionalParam(String args) { //to oldgameID's and another additional params. passed to the end of input
         String[] splitted = args.split("\\s+");
         return splitted[8];
--- ge/mziuri/network/ServerThread.java
+++ ge/mziuri/network/ServerThread.java
@@ -4,6 +4,10 @@ package ge.mziuri.network;
 import ge.mziuri.dao.BetDAOImpl;
 import ge.mziuri.dao.BookDAOImpl;
 import ge.mziuri.dao.GameDAOImpl;
+import ge.mziuri.exception.IncorrectGameException;
+import ge.mziuri.exception.NoSuchTeamException;
+import ge.mziuri.model.Game;
+import static ge.mziuri.network.Client.obj;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -31,8 +35,8 @@ public class ServerThread extends Thread {
         betdao = new BetDAOImpl();
         bookdao = new BookDAOImpl();
         try {
-            in = new ObjectInputStream(socket.getInputStream());
             out = new ObjectOutputStream(socket.getOutputStream());
+            in = new ObjectInputStream(socket.getInputStream());
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
@@ -52,7 +56,53 @@ public class ServerThread extends Thread {
         while (socket.isConnected()) {
             try {
                 Object[] obj = (Object[]) in.readObject();
-                sendCommand(obj);
+                        while (true) {
+                            GameDAOImpl gamedao = new GameDAOImpl();
+                            Object[] ans = new Object[3];
+                            switch((String)obj[0]) {
+                                case "game 1": 
+                                    gamedao.addGame((Game)obj[1]);
+                                    ans[0] = "game 1";
+                                    ans[1] = "game added successfully";
+                                    sendCommand(ans);
+                                    break;
+                                case "game 2":
+                                    gamedao.deleteGame((Integer)obj[1]);
+                                    ans[0] = "game 2";
+                                    ans[1] = "game deleted successfully";
+                                    sendCommand(ans);
+                                    break;
+                                case "game 3":
+                                    gamedao.editGame(Integer.parseInt((String)obj[1]), (Game)obj[2]);
+                                    ans[0] = "game 3";
+                                    ans[1] = "game edited successfully";
+                                    sendCommand(ans);
+                                case "game 4":
+                                    ans[0] = "game 4";
+                                    ans[1] = "entered 'about games'";
+                                    ans[2] = gamedao.aboutGames();
+                                    sendCommand(ans);
+                                    break;
+                                case "game 5":
+                                    ans[0] = "game 5";
+                                    ans[1] = "entered 'get date' ";
+                                    ans[2] = gamedao.getDate((Integer)obj[1]);
+                                    sendCommand(ans);
+                                    break;
+                                case "game 6":
+                                    try {
+                                        ans[0] = "game 6";
+                                        ans[1] = "entered 'get coefficient' ";
+                                        ans[2] = gamedao.getCoefficient((String)obj[1], (Game)obj[2]);
+                                    }
+                                    catch(IncorrectGameException | NoSuchTeamException ex) {
+                                        System.out.println("incorrect game or no such team");
+                                    }
+                                break;
+                                    
+                                    
+                            }
+                        }
                 
             } catch(IOException | ClassNotFoundException ex) {
                 System.out.println(ex.getMessage());
